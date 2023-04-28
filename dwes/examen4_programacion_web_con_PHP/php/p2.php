5734fce4520201753f56fd618456267cd1be4418
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2023-04-28 13:06:30 +0200

    p2

---
+++ dwes/examen4_programacion_web_con_PHP/php/p2.php
@@ -0,0 +1,8 @@
+
+
+
+<?php
+#redirige la petición realizada en index.php a la web www.apuntesinformaticafp.com
+    header("Location: https://www.apuntesinformaticafp.com/" . $_REQUEST['direccion']);
+?>
+ 
